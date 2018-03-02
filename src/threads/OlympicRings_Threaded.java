cc7c4a9d85fde7b771798ac59c1702394ef56913
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-01 16:57:49 -0800

    Finished with olympic rings. Onto brute force cracker next

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,40 @@
 package threads;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
 
-}
+public static void main(String[] args) {
+Robot blue = new Robot(150, 150);
+Robot yellow = new Robot(250,300);
+Robot black = new Robot(350,150);
+Robot green = new Robot(450,300);
+Robot red = new Robot(550,150);
+blue.setPenColor(Color.BLUE);
+yellow.setPenColor(Color.YELLOW);
+black.setPenColor(Color.BLACK);
+green.setPenColor(Color.green);
+red.setPenColor(Color.red);
+new Thread(()-> DrawCircle(blue, 0, 0, 255)).start();
+new Thread(()-> DrawCircle(yellow, 225, 225, 0)).start();
+new Thread(()-> DrawCircle(black, 0, 0, 0)).start();
+new Thread(()-> DrawCircle(green, 0, 255, 0)).start();
+new Thread(()-> DrawCircle(red, 225, 0, 0)).start();
 
+}
+static void DrawCircle(Robot robot, int r, int g, int b) {
+//robot.setPenColor(r,g,b);
+robot.setSpeed(10);
+robot.setPenWidth(3);
+robot.penDown();
+	for (int i = 0; i < 360; i++) {
+		robot.move(2);
+		robot.turn(1);
+	}
+robot.penUp();
+robot.moveTo(400, 600);
+}
+}
