25df309e8df875e1bee55533e67c83272c435922
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-15 16:58:24 -0700

    Working on ThreadedReverseGreeting

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -18,34 +18,43 @@ public class BruteForceCracker {
 	static float elapsedTime;
 
 	public static void main(String[] args) {
-		Thread one = new Thread(() -> crackCode(0, 1));
+		Thread zero = new Thread(() -> crackCode(0, 0));
+		Thread one = new Thread(() -> crackCode(100000000, 1));
+		Thread three = new Thread(() -> crackCode(300000000, 3));
+		Thread five = new Thread(() -> crackCode(500000000, 5));
+		Thread seven = new Thread(() -> crackCode(700000000, 7));
 		Thread two = new Thread(() -> crackCode(200000000, 2));
-		Thread three = new Thread(() -> crackCode(400000000, 3));
-		Thread four = new Thread(() -> crackCode(600000000, 4));
-		Thread five = new Thread(() -> crackCode(800000000, 5));
-		
-		System.out.println("Starting Brute Force Checker");
-		System.out.println(code);
-		startTime = System.currentTimeMillis();
-		
+		Thread four = new Thread(() -> crackCode(400000000, 4));
+		Thread six = new Thread(() -> crackCode(600000000, 6));
+		Thread eight = new Thread(() -> crackCode(800000000, 8));
+		Thread nine = new Thread(() -> crackCode(900000000, 9));
+
+		zero.start();
 		one.start();
 		two.start();
 		three.start();
 		four.start();
-	five.start();
+		five.start();
+		six.start();
+		seven.start();
+		eight.start();
+		nine.start();
+		
+		System.out.println("Starting Brute Force Checker");
+		System.out.println(code);
+		startTime = System.currentTimeMillis();
 	}
 
 	static void crackCode(int start, int num) {
 		while (!checkCode(start)) {
 			start++;
 		}
-	endTime = System.currentTimeMillis();
-	elapsedTime = (float) (endTime - startTime);
-	elapsedTime /= 1000.f;
-	System.out.println("Total time taken: " + elapsedTime + " seconds from codeCracker" + num);
-	System.out.println(start);
-			//System.out.println(start + " from " + num);
-		}
+		endTime = System.currentTimeMillis();
+		elapsedTime = (float) (endTime - startTime);
+		elapsedTime /= 1000.f;
+		System.out.println("Total time taken: " + elapsedTime + " seconds from codeCracker" + num);
+		System.out.println(start);
+	}
 
 	public static boolean checkCode(long p) {
 		if (p == code) {
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,26 @@
+package threads;
+
+import java.util.Stack;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+	Stack<Thread> th = new Stack<Thread>();
+
+	static Thread g;
+
+	public static void main(String[] args) {
+
+	}
+
+	static void createThread(int amount) {
+	
+	System.out.println("Hello From Thread " + amount + "!");
+	new Thread();
+
+	}
 }
