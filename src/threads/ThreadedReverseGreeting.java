e4538dc7abf4845b5539ecbb2f902b2e6d61f6d5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-22 16:43:53 -0700

    Module 4 Complete

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,5 @@
 package threads;
 
-import java.util.Stack;
-
 public class ThreadedReverseGreeting {
 	// Write a program that creates a thread (Thread 1) that creates another thread
 	// (Thread 2);
@@ -9,18 +7,15 @@ public class ThreadedReverseGreeting {
 	// Each thread should print "Hello from Thread <num>!",
 	// but you should structure your program such that the threads print their
 	// greetings in reverse order.
-	Stack<Thread> th = new Stack<Thread>();
-
-	static Thread g;
-
 	public static void main(String[] args) {
-
+		createThread(0);
+		System.out.println("the end");
 	}
-
 	static void createThread(int amount) {
-	
-	System.out.println("Hello From Thread " + amount + "!");
-	new Thread();
-
-	}
-}
+	Thread w = new Thread(()->{
+		if (amount<50) {createThread(amount+1);}
+	System.out.println("Hello From Thread " + amount + "!"); });
+	w.start();
+	try {
+		w.join();
+	}catch(Exception e){}}}
